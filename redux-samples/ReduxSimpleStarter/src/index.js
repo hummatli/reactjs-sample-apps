7c7e99227d56edb0eae5bb1cd2440d55636d5047
Sattar Hummatli <settarxan@gmail.com>
2017-12-02 17:06:29 +0400

    first call to youtube api

--- redux-samples/ReduxSimpleStarter/src/index.js
+++ redux-samples/ReduxSimpleStarter/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDv9mjWf9rxiqPZWe2TAzi4_G4V63i3TQU';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data)
+})
 
 const App = () => {
   return (
