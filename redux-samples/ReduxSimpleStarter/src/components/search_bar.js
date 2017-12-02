a7453f468e667d24fd7be4f66fbb144a7d0e7fb7
Sattar Hummatli <settarxan@gmail.com>
2017-12-02 20:41:55 +0400

    add all componenets

--- redux-samples/ReduxSimpleStarter/src/components/search_bar.js
+++ redux-samples/ReduxSimpleStarter/src/components/search_bar.js
@@ -5,16 +5,22 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
+    this.onInputChange = this.onInputChange.bind(this);
+
     this.state = { term: ''};
+  }
+
+  onInputChange(term) {
+    this.setState({ term })
+    this.props.onSearchTermChange(term);
   }
 
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input
           value={this.state.term}
-          onChange={ event => this.setState({ term: event.target.value }) }/>
-        Value of input: {this.state.term}
+          onChange={ event => this.onInputChange(event.target.value) }/>
       </div>
     )
   }
